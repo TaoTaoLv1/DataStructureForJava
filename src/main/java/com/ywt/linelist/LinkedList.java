9df5a13234f6053465f7137c0d2760204338a7c6
YwT <1181339151@qq.com>
2018-11-11 22:20:43 +0800

    [add] 链表

---
+++ src/main/java/com/ywt/linelist/LinkedList.java
@@ -0,0 +1,156 @@
+package com.ywt.linelist;
+
+/**
+ * @description: 链表
+ * @author: YwT
+ * @create: 2018-10-19 15:29
+ **/
+public class LinkedList<T> {
+
+    private class Node{
+        public T t;
+        public Node next;
+
+        public Node(T t, Node next) {
+            this.t = t;
+            this.next = next;
+        }
+
+        public Node(T t){
+            this(t, null);
+        }
+
+        public Node(){
+            this(null,null);
+        }
+
+        @Override
+        public String toString() {
+            return t.toString();
+        }
+    }
+
+    private Node dummyHead;
+    private int size;
+
+    public LinkedList() {
+        dummyHead = new Node(null, null);
+        size = 0;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+
+    public void add(int index, T t){
+        if (index < 0 || index > size){
+            throw new IllegalArgumentException("index 参数不对");
+        }
+
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++){
+            prev = prev.next;
+        }
+
+//            Node node = new Node(t);
+//            node.next = prev.next;
+//            prev.next = node;
+
+        prev.next = new Node(t, prev.next);
+        size ++;
+
+    }
+
+    public void addLast(T t){
+        add(size, t);
+    }
+
+    public void addFirst(T t){
+        add(0, t);
+    }
+
+    public T get(int index){
+        if (index < 0 || index > size){
+            throw new IllegalArgumentException("index 参数不对");
+        }
+
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++){
+            cur = cur.next;
+        }
+
+        return cur.t;
+    }
+
+    public T getFirst(){
+        return get(0);
+    }
+
+    public T getLast(){
+        return get(size - 1);
+    }
+
+    public void set(int index, T t){
+        if (index < 0 || index >= size){
+            throw new IllegalArgumentException("index 参数不对");
+        }
+
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++){
+            cur = cur.next;
+        }
+        cur.t = t;
+    }
+
+    public boolean contains(T t){
+        Node cur = dummyHead.next;
+        while (cur != null){
+            if (cur.t.equals(t)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public T remove(int index){
+        if (index < 0 || index >= size){
+            throw new IllegalArgumentException("index 参数不对");
+        }
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++){
+            prev = prev.next;
+        }
+
+        Node retNode = prev.next;
+        prev.next = retNode.next;
+        retNode.next = null;
+        size --;
+        return retNode.t;
+
+    }
+
+    public T removeFirst(){
+        return remove(0);
+    }
+
+    public T removeLast(){
+        return remove(size - 1);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        Node cur = dummyHead.next;
+        while (cur != null){
+            res.append(cur + "->" );
+            cur = cur.next;
+        }
+        res.append("null");
+        return res.toString();
+    }
+}
---
+++ src/main/java/com/ywt/linelist/Main.java
@@ -0,0 +1,23 @@
+package com.ywt.linelist;
+
+/**
+ * @description:
+ * @author: YwT
+ * @create: 2018-10-19 16:05
+ **/
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        for (int i = 0; i < 5; i++){
+            linkedList.addFirst(i);
+            System.out.println(linkedList);
+        }
+
+        linkedList.add(2, 777);
+        System.out.println(linkedList);
+
+        linkedList.remove(2);
+        System.out.println(linkedList);
+    }
+}
