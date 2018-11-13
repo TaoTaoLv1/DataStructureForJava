7d244c248cf42de22da1d0ee20cd8e2e893af515
YwT <1181339151@qq.com>
2018-11-13 15:01:58 +0800

    [add] 添加二分搜索树 -- 添加元素

--- src/main/java/com/ywt/BinarySearchTree/BST.java
+++ src/main/java/com/ywt/BinarySearchTree/BST.java
@@ -33,4 +33,34 @@ public class BST<T extends Comparable<T>>{
     public boolean isEmpty(){
         return size == 0;
     }
+
+    public void add(T e){
+        if (root == null){
+            root = new Node(e);
+        }else {
+            add(root, e);
+        }
+    }
+
+    private void add(Node node, T e){
+        if (e.equals(node.e)){
+            return;
+        }else if (e.compareTo(node.e) < 0){
+            if (node.left == null){
+                node.left = new Node(e);
+                size ++;
+                return;
+            }else {
+                add(node.left, e);
+            }
+        }else if (e.compareTo(node.e) > 0){
+            if (node.right == null){
+                node.right = new Node(e);
+                size ++;
+                return;
+            }else {
+                add(node.right, e);
+            }
+        }
+    }
 }
