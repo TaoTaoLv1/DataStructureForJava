8b6f59f1238d0e8104b9287dcdc2c53c19939ffb
YwT <1181339151@qq.com>
2018-11-26 11:45:34 +0800

    [add] 线段树 查询方法

--- src/main/java/com/ywt/segmenttree/SegmentTree.java
+++ src/main/java/com/ywt/segmenttree/SegmentTree.java
@@ -24,7 +24,7 @@ public class SegmentTree<T> {
      * 在 treeIndex 位置的创建 表示[leftIndex....rightIndex] 的区间树
      * @param treeIndex   区间树位置下标
      * @param leftIndex 数组的左边界
-     * @param rightIndex 数组的有边界
+     * @param rightIndex 数组的右边界
      */
     private void buildSegmentTree(int treeIndex, int leftIndex, int rightIndex){
         if (leftIndex == rightIndex){
@@ -61,6 +61,49 @@ public class SegmentTree<T> {
     private int leftChild(int index){
         return index * 2 + 1;
     }
+
+    /**
+     * @param queryL  数组左边界
+     * @param queryR  数组右边界
+     * @return  返回【L....R】的值
+     */
+    public T query(int queryL, int queryR){
+        if(queryL < 0 || queryL >= data.length ||
+                queryR < 0 || queryR >= data.length || queryL > queryR)
+            throw new IllegalArgumentException("索引超标");
+        return query(0, 0, data.length -1, queryL, queryR);
+    }
+
+    /**
+     *
+     * @param treeIndex  线段树的根
+     * @param l           线段树的左边界
+     * @param r           线段树的右边界
+     * @param queryL     待查询的左边界
+     * @param queryR     待查询的右边界
+     * @return
+     */
+    private T query(int treeIndex, int l, int r, int queryL, int queryR){
+        if (l == queryL && r == queryR){
+            return tree[treeIndex];
+        }
+
+        int mid = l + (r - l) / 2;
+        int leftChildTree = leftChild(treeIndex);
+        int rightChildTree = rightChild(treeIndex);
+
+        if (queryL >= mid + 1){
+            return query(rightChildTree, mid + 1, r, queryL, queryR);
+        }else if (queryR <= mid){
+            return query(leftChildTree, l, mid, queryL, queryR);
+        }
+
+        T leftResult = query(leftChildTree, l, mid, queryL, mid);
+        T rightResult = query(rightChildTree, mid + 1, r, mid + 1, queryR);
+
+        return merger.merge(leftResult, rightResult);
+    }
+
 
     /**
      * 返回当前节点的 右孩子索引
